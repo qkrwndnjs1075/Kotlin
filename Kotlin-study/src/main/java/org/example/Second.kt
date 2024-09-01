2cb24d08b48243652e891b35cf8f539f1d15d8f9
박주원 <qkrwndnjs7308@gmail.com>
2024-09-01 23:17:53 +0900

    Second

--- Kotlin-study/src/main/java/org/example/Second.kt
+++ Kotlin-study/src/main/java/org/example/Second.kt
@@ -5,7 +5,7 @@ package org.example
      var a = 1234
      var b = 1234L
 
-     var c = 12.45L
+     var c = 12.45
      var d = 12.45f
 
      var e = 0xABCD
@@ -15,6 +15,12 @@ package org.example
      var h = 'c'
 
      println(add(5,6,7))
+
+     doWhen(1)
+     doWhen("park")
+     doWhen("냠냠")
+
+
  }
 
     fun add(a: Int,b:Int,c:Int) = a+b+c
@@ -25,3 +31,18 @@ package org.example
         return a + b + c
 
     }*/
+
+    fun doWhen (a: Any){
+
+        when(a){
+
+            1 -> println("정수 1입니다")
+            "park" -> println("박주원입니다")
+            is Long -> ("Long 타입 입니다")
+            !is String ->("String 타입이 아닙니다")
+            else -> println("다 아니다")
+
+
+        }
+
+    }
