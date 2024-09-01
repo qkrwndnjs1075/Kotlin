b70e946307d8283054b60ac8ce328261d0fbb9d8
박주원 <qkrwndnjs7308@gmail.com>
2024-09-01 22:38:52 +0900

    Kotlin - 1

---
+++ Kotlin-study/src/main/java/org/example/First.kt
@@ -0,0 +1,22 @@
+package org.example
+
+
+    fun main(){
+
+        var a: Int = 10
+
+        var b: Long = a.toLong()
+
+        var c: Int? = null
+
+        var intArr =  arrayOf(1,2,3,4,5)
+
+        var nullArr = arrayOfNulls<Int>(5)
+
+        intArr[2] = 8
+
+        println(intArr[4])
+
+    }
+
+
