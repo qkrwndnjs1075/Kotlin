3c0f73a41667350d570309fefad7e4b4c75b4c0d
박주원 <qkrwndnjs7308@gmail.com>
2024-09-01 23:01:52 +0900

    Second

--- Kotlin-study/src/main/java/org/example/Second.kt
+++ Kotlin-study/src/main/java/org/example/Second.kt
@@ -2,8 +2,26 @@ package org.example
 
  fun main(){
 
-     
+     var a = 1234
+     var b = 1234L
 
+     var c = 12.45L
+     var d = 12.45f
 
+     var e = 0xABCD
+     var f = 0b010101010
 
+     var g = true
+     var h = 'c'
+
+     println(add(5,6,7))
  }
+
+    fun add(a: Int,b:Int,c:Int) = a+b+c
+
+
+    /*fun add(a: Int, b: Int, c:Int): Int{
+
+        return a + b + c
+
+    }*/
