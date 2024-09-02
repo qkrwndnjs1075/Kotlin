0f9b836a1e11dbed34aa06238f315fba567de20c
박주원 <qkrwndnjs7308@gmail.com>
2024-09-02 22:56:16 +0900

    .

--- Kotlin-study/src/main/java/org/example/4.kt
+++ Kotlin-study/src/main/java/org/example/4.kt
@@ -6,4 +6,11 @@ fun main(){
         println(i)
     }
 
+    loop@for(i in 1..10){
+        for(j in 1..10){
+            if(i == 1 && j == 2) break@loop
+            println("i: $i, j: $j")
+        }
+    }
+
 }
