d7710e628391eb9a50add1156d585bb61df68bf6
박주원 <qkrwndnjs7308@gmail.com>
2024-10-22 23:16:37 +0900

    // 왼쪽이 this

--- Kotlin-study/src/main/java/org/example/infix2.kt
+++ Kotlin-study/src/main/java/org/example/infix2.kt
@@ -2,6 +2,9 @@ package org.example
 
 fun main(){
 
+    println(6 multiply 4) // 왼쪽이 this 우측이 패터미터인 x에 해당한다.
+
+    println(6.multiply(4))
 
 }
-infix fun Int.multiply(x:Int):Int = Int = this * x 
+infix fun Int.multiply(x:Int): Int = this * x
