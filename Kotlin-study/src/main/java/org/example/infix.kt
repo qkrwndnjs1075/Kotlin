3d0a3c53b0c20fde9aa871e855788bb4cbc183c2
박주원 <qkrwndnjs7308@gmail.com>
2024-10-22 23:04:04 +0900

    코틀린...

--- Kotlin-study/src/main/java/org/example/infix.kt
+++ Kotlin-study/src/main/java/org/example/infix.kt
@@ -5,6 +5,8 @@ fun main(){
     deliveryItem("짬뽕")
     deliveryItem("책", 3)
 
+    deliveryItem("선물", destination = "친구집")
+
 
 }
 fun deliveryItem(name: String, count: Int = 1, destination: String = "집"){
