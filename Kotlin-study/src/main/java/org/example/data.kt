85e7f072a466bd89cb289a9b9b0df1c0e7a8b005
박주원 <qkrwndnjs7308@gmail.com>
2024-11-11 17:01:22 +0900

    d

--- Kotlin-study/src/main/java/org/example/data.kt
+++ Kotlin-study/src/main/java/org/example/data.kt
@@ -1,2 +1,15 @@
 package org.example
 
+fun main(){
+
+    val list = listOf(Data("보영",232),
+                        Data("루다",304))
+
+    for((a,b) in list ){
+        println("${a}, ${b}")
+    }
+}
+
+class General(val name: String, val id: Int)
+
+data class Data(val name: String, val id: Int)
